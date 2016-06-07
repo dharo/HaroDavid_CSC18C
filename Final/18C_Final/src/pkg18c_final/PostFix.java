ee8fa022c939cbb1be1d514002bcd96eed0e6699
David Haro <david.haro@me.com>
2016-06-07 16:26:52 -0700

    PSP Update

--- Final/18C_Final/src/pkg18c_final/PostFix.java
+++ Final/18C_Final/src/pkg18c_final/PostFix.java
@@ -104,135 +104,135 @@ public class PostFix {
     
     
     
-    private class Stack<T> {
-    private Node<T> head=null;
-    private int items;
-    private int size;
+        private class Stack<T> {
+        private Node<T> head=null;
+        private int items;
+        private int size;
 
-    //automatically make only an empty stack, just push and pop accordingly
-    public Stack() {
-        //default size of 2
-        this.items = 0;
-        this.size = 2;
-    }
-
-    public Stack(int InitialStackSize) {
-        //items = number of items currently stored in the stack
-        //size = number of items stack can hold
-        this.items = 0;
-        this.size = InitialStackSize;
-    }
-
-
-    public boolean isEmpty() {
-        return this.items == 0;
-    }
-
-    public boolean isFull() {
-        //if # of items has reached size it is full
-        if(this.items == 0) return false;
-        return (this.size == this.items );
-    }
-
-    public T peek(){
-        T top = null;
-
-        if (head != null){
-            top = head.getValue();
+        //automatically make only an empty stack, just push and pop accordingly
+        public Stack() {
+            //default size of 2
+            this.items = 0;
+            this.size = 2;
         }
-        return top;
-    }
 
-    public void push (T data){
-        if (isEmpty()) {
-            head = new Node<T>(data);
-            //tail=head; // when first item is enqueued, head and tail are the same
-            this.items++; // increment the number of items in the queue
+        public Stack(int InitialStackSize) {
+            //items = number of items currently stored in the stack
+            //size = number of items stack can hold
+            this.items = 0;
+            this.size = InitialStackSize;
         }
-        else{
-            if(isFull()) {
-                this.size++;
-                //System.out.println("Stack Limit reached: Increasing stack size");
+
+
+        public boolean isEmpty() {
+            return this.items == 0;
+        }
+
+        public boolean isFull() {
+            //if # of items has reached size it is full
+            if(this.items == 0) return false;
+            return (this.size == this.items );
+        }
+
+        public T peek(){
+            T top = null;
+
+            if (head != null){
+                top = head.getValue();
             }
-            Node top = new Node<T>(data);
-            top.setNext(head);
-            head = top;
-            items++;
+            return top;
         }
-    }
 
-    public T pop(){
-        T top = peek();
-        if (head != null){
-            head = head.getNext();
-            items--;
-            size--;
+        public void push (T data){
+            if (isEmpty()) {
+                head = new Node<T>(data);
+                //tail=head; // when first item is enqueued, head and tail are the same
+                this.items++; // increment the number of items in the queue
+            }
+            else{
+                if(isFull()) {
+                    this.size++;
+                    //System.out.println("Stack Limit reached: Increasing stack size");
+                }
+                Node top = new Node<T>(data);
+                top.setNext(head);
+                head = top;
+                items++;
+            }
         }
-        return top;
-    }
 
-    public int getSize(){
-        //size = number of items stack can hold
-        return this.size;
-    }
-    public int getItems(){
-        //items = number of items currently stored in the stack
-        return this.items;
-    }
-    public void display() {
-        System.out.print("Current view of the stack: ");
-        Node current=head;
-        for(int i=0;i<items;i++) {
-            System.out.print(current.getValue()+" ");
-            current=current.getNext();
-            if ( current==null ) break;
+        public T pop(){
+            T top = peek();
+            if (head != null){
+                head = head.getNext();
+                items--;
+                size--;
+            }
+            return top;
         }
-        System.out.println();
+
+        public int getSize(){
+            //size = number of items stack can hold
+            return this.size;
+        }
+        public int getItems(){
+            //items = number of items currently stored in the stack
+            return this.items;
+        }
+        public void display() {
+            System.out.print("Current view of the stack: ");
+            Node current=head;
+            for(int i=0;i<items;i++) {
+                System.out.print(current.getValue()+" ");
+                current=current.getNext();
+                if ( current==null ) break;
+            }
+            System.out.println();
+        }
+    ///////////////////Private Node Class ///////////////////////////
+        //because arrays are whatever...
+        private class Node<T> {
+            private T value;
+            private Node _previous, _next;
+
+            public Node(T data) {
+                value = data;
+                _previous = null;
+                _next = null;
+            }
+
+            protected Node(T data, Node previousNode, Node nextNode) {
+                value = data;
+                _previous = previousNode;
+                _next = nextNode;
+            }
+
+            public Node getNext() {
+                return _next;
+            }
+
+            public Node getPrevious() {
+                return _previous;
+            }
+
+            public void setValue(T newValue)
+            {
+                value=newValue;
+            }
+
+            public T getValue() {
+                return value;
+            }
+
+            public void setNext(Node newNextNode) {
+                _next = newNextNode;
+            }
+
+            public void setPrevious(Node newPreviousNode) {
+                _next = newPreviousNode;
+            }
+        }
+
+
     }
-///////////////////Private Node Class ///////////////////////////
-    //because arrays are whatever...
-private class Node<T> {
-    private T value;
-    private Node _previous, _next;
-
-    public Node(T data) {
-        value = data;
-        _previous = null;
-        _next = null;
-    }
-
-    protected Node(T data, Node previousNode, Node nextNode) {
-        value = data;
-        _previous = previousNode;
-        _next = nextNode;
-    }
-
-    public Node getNext() {
-        return _next;
-    }
-
-    public Node getPrevious() {
-        return _previous;
-    }
-
-    public void setValue(T newValue)
-    {
-        value=newValue;
-    }
-
-    public T getValue() {
-        return value;
-    }
-
-    public void setNext(Node newNextNode) {
-        _next = newNextNode;
-    }
-
-    public void setPrevious(Node newPreviousNode) {
-        _next = newPreviousNode;
-    }
-}
-
-
-}
 }
