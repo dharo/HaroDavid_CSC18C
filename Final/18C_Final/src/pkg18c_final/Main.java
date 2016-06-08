5ca447e21c4eefc158c52f3dad55df3fc590d7f8
David Haro <david.haro@me.com>
2016-06-07 17:01:55 -0700

    changed variable names to match possible unit tests

--- Final/18C_Final/src/pkg18c_final/Main.java
+++ Final/18C_Final/src/pkg18c_final/Main.java
@@ -14,14 +14,14 @@ public class Main {
     public static void main(String[] args) {
        String infix =  "6 * (3+(7*8)*(5+2))";
        PostFix post = new PostFix();
-       String output;
+       String expr;
        double result;
        
-       output = post.convertToPostFix(infix);
-       result = post.evaluatePostFix(output);
+       expr = post.convertToPostFix(infix);
+       result = post.evaluatePostFix(expr);
        
        System.out.println("Infix notation "+infix);
-       System.out.println("PostFix notation of infix: "+output);
+       System.out.println("PostFix notation of infix: "+expr);
        System.out.println("PostFix notation evaluates to: "+result);
     }
 
